0072bfaf48e30a432fee1f0a2fc64785cce8eb07
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2023-11-06 18:35:24 +0530

    Added api endpoint

--- src/components/Histogram.jsx
+++ src/components/Histogram.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-
 const Tooltip = ({ text, top, left }) => {
     return (
         <div
@@ -13,75 +12,70 @@ const Tooltip = ({ text, top, left }) => {
 };
 
 const Histogram = () => {
-    // State to store the data
     const [data, setData] = useState([]);
     const [tooltip, setTooltip] = useState(null);
-
-    // State to track the hover state for each bar
     const [hoveredBar, setHoveredBar] = useState(null);
-
-    // State to track whether data is generated
     const [dataGenerated, setDataGenerated] = useState(false);
 
-    // Function to fetch and regenerate data
     const regenerateData = () => {
-        // generate random data
-        const randomData = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
-        setData(randomData);
-        setDataGenerated(true);
+        fetch('https://www.random.org/integers/?num=200&min=1&max=10&col=1&base=10&format=plain&rnd=new')
+            .then(response => response.text())
+            .then(data => {
+                const parsedData = data.split('\n').map(Number);
+                setData(parsedData);
+                setDataGenerated(true);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     };
 
-    // Initial data (can be empty)
-    const initialData = [];
+    const numBins = 10;
 
-    // Determine the number of bins or categories
-    const numBins = 10; // Adjust as needed
-
-    // Calculate bin width and set up bins
-    const binWidth = dataGenerated ? 400 / numBins : 0;
+    const binWidth = dataGenerated ? 100 / numBins : 0;
     const bins = Array.from({ length: numBins }, (_, i) => ({
         start: dataGenerated ? i * binWidth : 0,
         end: dataGenerated ? (i + 1) * binWidth : 0,
         count: 0,
     }));
 
-    // Fill the bins with data counts
     data.forEach((value) => {
-        const binIndex = Math.floor((value / 100) * numBins);
+        const binIndex = Math.floor(value / 10 * numBins);
         if (binIndex >= 0 && binIndex < numBins) {
             bins[binIndex].count++;
         }
     });
 
-    // Find the maximum count for scaling
     const maxCount = Math.max(...bins.map((bin) => bin.count));
 
+    const xLabels = Array.from({ length: numBins }).map((_, i) => ({
+        label: i.toString(), // Start from 0
+    }));
+
     return (
-        <div className="flex mt-32 flex-col items-center ">
+        <div className="flex mt-32 flex-col items-center">
             <div className="max-w-full bg-white rounded-lg shadow-2xl mt-24 p-4 md:p-6 relative">
                 <div className="pb-4 mb-4">
                     <div className="grid grid-cols-10 gap-2">
                         {bins.map((bin, index) => (
-                            <div
-                                key={index}
-                                className="relative mt-16"
-                            >
+                            <div key={index} className="relative mt-16">
                                 <div
                                     className="absolute bottom-0 left-0 right-0 rounded cursor-pointer"
                                     style={{
-                                        height: `${45 + (bin.count / maxCount) * (100 - 45)}px`, 
+                                        height: `${45 + (bin.count / maxCount) * (100 - 45)}px`,
                                         minHeight: '0px',
-                                        background: hoveredBar === index ? 'rgba(255, 99, 132, 0.5)' : 'rgba(53, 162, 235, 0.5)',
+                                        background: hoveredBar === index
+                                            ? 'rgba(255, 99, 132, 0.5)'
+                                            : 'rgba(53, 162, 235, 0.5',
                                     }}
                                     onMouseEnter={() => setHoveredBar(index)}
-                                    onMouseLeave={() => setHoveredBar(index)}
+                                    onMouseLeave={() => setHoveredBar(null)}
                                 >
-                                    
                                     {hoveredBar === index && (
                                         <Tooltip
                                             text={`${bin.count}`}
-                                            top={-20} // Adjust the position as needed
-                                            left={0} // Adjust the position as needed
+                                            top={-20}
+                                            left={0}
                                         />
                                     )}
                                 </div>
@@ -90,11 +84,10 @@ const Histogram = () => {
                                 </div>
                             </div>
                         ))}
-
                     </div>
                     <div className="absolute bottom-0 left-0 w-full h-px bg-gray-600"></div>
                     <div className="absolute bottom-0 left-0 w-px h-full bg-gray-600"></div>
-                    {Array.from({ length: numBins }).map((_, i) => (
+                    {xLabels.map((label, i) => (
                         <div
                             key={i}
                             className="absolute top-52 bottom-0 left-0"
@@ -104,12 +97,12 @@ const Histogram = () => {
                             }}
                         >
                             <div className="text-center text-gray-600">
-                                {(i / numBins).toFixed(2)}
+                                {label.label}
                             </div>
                         </div>
                     ))}
                     <div className="absolute -left-14 top-16 bottom-0 transform translateX(-50%) translatey(-50%)">
-                        <div className="text-center transform rotate-90 mt-2  text-gray-600">
+                        <div className="text-center transform rotate-90 mt-2 text-gray-600">
                             Frequency
                         </div>
                     </div>
@@ -124,7 +117,6 @@ const Histogram = () => {
                 </button>
             </div>
         </div>
-
     );
 };
 
